b8820e566d467b662234d97329c4a23bd2a869e5
noaadevops <noahdesisa@gmail.com>
2024-10-14 21:29:13 -0400

    Update user_data.sh

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install nginx -y
-sudo echo "Hi cloud sheger you are on prod server" > /var/www/html/index.nginx-debian.html
+sudo echo "Hi noa you are on prod server" > /var/www/html/index.nginx-debian.html
